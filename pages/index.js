eab07ecab1dedd5d902c28d17eb328ab82d7a308
Henry <lkhh@protonmail.com>
2022-04-20 03:08:24 +0800

    Add profile picture and fix styles

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,14 @@ export default function Home() {
   return (
     <>
       <button
-        className="absolute top-0 left-0 print:hidden bg-yellow-400 p-1 pb-2 pr-2 rounded-br-full"
+        className="absolute top-0 left-0 print:hidden bg-yellow-400 p-2 pb-3 pr-3 rounded-br-full"
         onClick={() => window.print()}
       >
         <PrinterIcon className="h-6 w-6 shrink-0 text-black" />
       </button>
 
       <button
-        className="absolute top-0 right-0 print:hidden bg-yellow-400 p-1 pb-2 pl-2 rounded-bl-full"
+        className="absolute top-0 right-0 print:hidden bg-yellow-400 p-2 pb-3 pl-3 rounded-bl-full"
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       >
         {theme === "light" ? (
@@ -37,12 +37,21 @@ export default function Home() {
 
       <div className="grid gap-8 p-5 max-w-2xl mx-auto">
         <section>
+          <Image
+            className="rounded-full"
+            src="/48958400.jpg"
+            alt="profile"
+            height={200}
+            width={200}
+          />
           <h1 className="text-4xl font-bold tracking-wider">Henry Li</h1>
           <h3>Immediately available</h3>
         </section>
 
         <section>
-          <h3 className="font-bold tracking-widest">CONNECT</h3>
+          <h3 className="font-bold tracking-widest underline decoration-2">
+            CONNECT
+          </h3>
           <p className="flex gap-1 items-center">
             <PhoneIcon className="h-4 w-4 shrink-0" />
             <span>63774207</span>
@@ -58,10 +67,12 @@ export default function Home() {
         </section>
 
         <section>
-          <h3 className="font-bold tracking-widest">LANGUAGES</h3>
+          <h3 className="font-bold tracking-widest underline decoration-2">
+            LANGUAGES
+          </h3>
           <div className="tooltip tooltip-bottom w-full" data-tip="Fluent">
             <p className="flex gap-1 items-center">
-              <GlobeAltIcon className="h-4 w-4 shrink-0" />
+              <GlobeAltIcon className="h-4 w-4 shrink-0 text-red-400" />
               <span className="-z-10">English</span>
               <span className="hidden print:inline">(Fluent)</span>
             </p>
@@ -73,7 +84,7 @@ export default function Home() {
           </div>
           <div className="tooltip tooltip-bottom w-full" data-tip="Native">
             <p className="flex gap-1 items-center">
-              <GlobeAltIcon className="h-4 w-4 shrink-0" />
+              <GlobeAltIcon className="h-4 w-4 shrink-0 text-indigo-400" />
               <span className="-z-10">Cantonese</span>
               <span className="hidden print:inline">(Native)</span>
             </p>
@@ -85,7 +96,7 @@ export default function Home() {
           </div>
           <div className="tooltip tooltip-bottom w-full" data-tip="Proficient">
             <p className="flex gap-1 items-center">
-              <GlobeAltIcon className="h-4 w-4 shrink-0" />
+              <GlobeAltIcon className="h-4 w-4 shrink-0 text-yellow-400" />
               <span className="-z-10">Mandarin</span>
               <span className="hidden print:inline">(Proficient)</span>
             </p>
@@ -98,13 +109,15 @@ export default function Home() {
         </section>
 
         <section>
-          <h3 className="font-bold tracking-widest">EDUCATION</h3>
+          <h3 className="font-bold tracking-widest underline decoration-2">
+            EDUCATION
+          </h3>
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold italic">The University of Manchester</p>
               <p className="flex gap-1 items-center">
-                <AcademicCapIcon className="h-4 w-4 shrink-0 shrink-0" /> BA
-                Economics
+                <AcademicCapIcon className="h-4 w-4 shrink-0 text-green-400" />{" "}
+                BA Economics
               </p>
             </div>
             <p className="italic text-sm shrink-0">Sep 12 - Jul 16</p>
@@ -112,7 +125,9 @@ export default function Home() {
         </section>
 
         <section>
-          <h3 className="font-bold tracking-widest">INTRODUCTION</h3>
+          <h3 className="font-bold tracking-widest underline decoration-2">
+            INTRODUCTION
+          </h3>
           <p>
             A software developer with a strong analytical mind seeking for a
             position in a dynamic and progressive company
@@ -120,8 +135,10 @@ export default function Home() {
         </section>
 
         <section>
-          <h3 className="font-bold tracking-widest">TECHNOLOGIES</h3>
-          <div className="flex items-center gap-2">
+          <h3 className="font-bold tracking-widest underline decoration-2">
+            TECHNOLOGIES
+          </h3>
+          <div className="flex items-center gap-2 mt-1">
             <div className="tooltip tooltip-bottom" data-tip="Typescript">
               <Image
                 src="/typescript-icon.svg"
@@ -159,7 +176,9 @@ export default function Home() {
         </section>
 
         <section>
-          <h3 className="font-bold tracking-widest">EXPERIENCES</h3>
+          <h3 className="font-bold tracking-widest underline decoration-2">
+            EXPERIENCES
+          </h3>
           <div className="flex flex-col gap-5">
             <div>
               <div className="flex justify-between items-center">
